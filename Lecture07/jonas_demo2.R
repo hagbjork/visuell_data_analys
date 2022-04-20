4b54278e1ee3615d76688f19621c89c06e652d03
hagbjork <jonas@hagbjork.se>
2022-04-20 11:26:45 +0200

    add some cheat sheats to folder + demo file lecture 10

--- Lecture07/jonas_demo2.R
+++ Lecture07/jonas_demo2.R
@@ -18,3 +18,26 @@ boolean_df <- as.data.frame(rowSum(is.na(data)))
 colnames(boolean_df) <- 'na_rows'
 filter(boolean_df, na_rows > 0)
 
+data <- na.omit(data)
+
+str(data)
+
+set.seed(2)
+
+indices <- sample(2, nrows(data), replace = T, prob = c(0.7, 0.3))
+train <- data[indices == 1,]
+test <- data[indices == 2,]
+
+#Predikterat NSP o
+forest_classifier <- randomForest(NSP ~., data = train)
+print(forest_classifier)
+
+predictions <- predict(forest_classifier, test)
+
+head(predictions)
+
+cm <- confusionMatrix(predictions, test$NSP)
+cm
+cm$byClass
+
+varImpPlot(forets_classifier, sort = T, n.var = 10)
